a410b06020c77aab9102d67bc422bfeca8926b97
Lars Johan <larsjny@stud.ntnu.no>
2018-04-12 15:35:14 +0200

    #5 Created activity and design for loginscreen. Added icons for login with facebook, google and twitter

--- app/src/main/java/no/ntnu/diverslogbook/MainActivity.java
+++ app/src/main/java/no/ntnu/diverslogbook/MainActivity.java
@@ -1,7 +1,9 @@
 package no.ntnu.diverslogbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.b_testButton);
+
+        button.setOnClickListener(view -> {
+            Intent testLogin = new Intent(this, LoginActivity.class);
+            startActivity(testLogin);
+        });
+
     }
 }
