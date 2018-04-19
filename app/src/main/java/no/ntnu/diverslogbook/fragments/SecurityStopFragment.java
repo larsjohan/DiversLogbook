20889f33d50ff7d804d70f7739e1d45d42bb7a30
Linn-Hege Kristensen <linn.h.kristensen@ntnu.no>
2018-04-19 13:07:35 +0200

    Still setting up the plan view.

---
+++ app/src/main/java/no/ntnu/diverslogbook/fragments/SecurityStopFragment.java
@@ -0,0 +1,31 @@
+package no.ntnu.diverslogbook.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import no.ntnu.diverslogbook.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecurityStopFragment extends Fragment {
+
+
+    public SecurityStopFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_security_stop, container, false);
+    }
+
+}
